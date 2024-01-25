5887a206983ed9b02d70fba8b3481ab49fc6a256
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2024-01-26 01:00:39 +0530

    file upload stuff

---
+++ src/middlewares/multer-middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // file - original file to be uploaded!
+    cb(null, "./public/temp"); // destination where temp files are saved on server
+  },
+  filename: function (req, file, cb) {
+    // to add custom suffix to filename
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage: storage });
---
+++ src/utils/cloudinary-file-upload.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) throw new Error("Local file not found!");
+    // upload file on cloudinary!
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // url of the file uploaded to cloudinary
+    let fileUrl = response.url;
+    // file uploaded succesfully! now unlink temp local file.
+    return response.url;
+  } catch (error) {
+    // deleted file from our server in case of any file curruption!
+    fs.unlink(localFilePath);
+  }
+};
